c0d55362608fc50497c4db12dc3ef65e6f8d2046
Samir Shaw <mitchellsamir999@gmail.com>
2026-01-05 19:19:52 +0530

    719

--- 117.py
+++ 117.py
@@ -1,1 +1,12 @@
-#WAP
+#WAP to perform Anagram
+a='silent'
+b='silenl'
+if len(a)==len(b):
+    for i in a:
+        if i not in b:
+            print("not anagram")
+            break
+        else:
+            pass
+    print("anagram")
+            
