3f333dbed4e23fb2e845d7afdf40e3c19b3990d3
Samir Shaw <mitchellsamir999@gmail.com>
2026-01-09 20:03:46 +0530

    65656

--- 51.py
+++ 51.py
@@ -1,10 +1,10 @@
 # #WAP to find number 341 from 143 without using indexing and typecasting.
-a = 143 
-result = ((a%10)*100+(a%100)//10*10+a//100)
-print(result)
+# a = 143 
+# result = ((a%10)*100+(a%100)//10*10+a//100)
+# print(result)
 
 # #---------------------------OR---------------------------------
-a=143
+a=int(input("enter no for rev "))
 rev=0
 while a!=0:
     id = a%10
@@ -16,11 +16,11 @@ print(rev)
 # # Write a program using a while loop that keeps asking the user to enter numbers. 
 # # Stop when the total sum exceeds 100, and then print the sum and how many 
 # # numbers were entered
-total=0
-count=0
-while total<=100:
-    num= int(input("Enter a number: "))
-    total+=num
-    count+=1
-print(total)
-print(count)  
+# total=0
+# count=0
+# while total<=100:
+#     num= int(input("Enter a number: "))
+#     total+=num
+#     count+=1
+# print(total)
+# print(count)  
