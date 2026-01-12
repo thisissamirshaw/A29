9cc38d8048441511896ef25d97a77e315c087431
Samir Shaw <mitchellsamir999@gmail.com>
2026-01-13 01:03:25 +0530

    13th Morning

--- 82.py
+++ 82.py
@@ -1,12 +1,14 @@
 #WAP to ext all the int from a given list using isinstance().
-# lt=[123,458,'op','liy']
-# out=[]
-# for i in range(len(lt)):
-#     if(type(lt[i])==int):
-#         out+=lt[i]
-# print(out)  
-# for i in a:
-# if(isinstance(i,int)):
-# out+[i]
-# print(out)      
+lt=[123,458,'op','liy']
+out=[]
+for i in lt:
+    if(isinstance(i,int)):
+        out+=[i]
+print(out)
+
+
+# # for i in range(len(lt)):
+# #     if(type(lt[i])==int):
+# #         out+=lt[i]
+# print(out)    if isinstance(i,int):
     
