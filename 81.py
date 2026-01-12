88fdb7ff3e21710414fd0b30e40f3f05f9d3c433
Samir Shaw <mitchellsamir999@gmail.com>
2026-01-13 01:44:15 +0530

    144

--- 81.py
+++ 81.py
@@ -1,7 +1,44 @@
-#WAP to uppercase at even index
+#WAP to input a str print a str with uppercase at even index of the input str,
+#  without using any inbuilt library function upper() or lower() and dont use ord()
 a=input("enter a string")
 out=''
 for i in a:
-    if('A'<=i<='Z') and a:
-        out+=i
-print(out)  
+    if 'A'<=i<='Z':  #checking for uppercase
+        if a.index(i)%2==0:
+            out+=i
+print(out)
+    
+
+
+
+
+# for i in a:
+#     if a.index(i)%2==0:
+#         out+=chr(ord(i)-32)  #upper case conversion without using inbuilt function
+#     else:
+#         out+=chr(ord(i)+32)  #lower case conversion without using inbuilt function
+# print(out)
+
+
+
+
+# a=input("enter a string")
+# out=''
+# for i in a:
+#     if a.index(i)%2==0:
+#         out+=i.upper()
+#     else:
+#         out+=i.lower()
+# print(out)
+
+
+
+
+# a=input("enter a string")
+# out=''
+# for i in a:
+#     if a.index(i)%2==0:
+#         out+=i.upper()
+#     else:
+#         out+=i.lower()
+# print(out)
