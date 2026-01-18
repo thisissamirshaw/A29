8bcfe2300883d98f090b2747be3324e3cb5bf006
Samir Shaw <mitchellsamir999@gmail.com>
2026-01-18 21:21:41 +0530

    18921

--- 90.py
+++ 90.py
@@ -1,6 +1,7 @@
-#WAP
+#WAP to shift each character of a string by one position in the alphabet.
+# For example, 'a' becomes 'b', 'b' becomes 'c', ..., 'y' becomes 'z', and 'z' becomes 'a'.
 a='abcstqz'
-op='abcdefg'
+op='bcdtura'
 out=''
 for i in a:
     if i!='z':
