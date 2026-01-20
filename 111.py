89bf81b0cb9286af0a4260a29b2bd0d836b36029
Samir Shaw <mitchellsamir999@gmail.com>
2026-01-20 17:50:10 +0530

    550

--- 111.py
+++ 111.py
@@ -1,22 +1,17 @@
-#WAP 
+#i th arg and without written vals
+#WAP to create a tuple with elements and their lengths/count
 a=[1,'Hello',[1,2],3.8,3+5j]
 op=[(1,1),('Hello',5),([1,2],2),(3.8,1),(3+5j,1)]
-
 def tupp(a):
     out=[]
     for i in a:
-        if type(i)==int:
+        if type(i) in (int,float,complex):
             out.append((i,1))
-        elif type(i)==str:
+        elif type(i) in (str,list):
             out.append((i,len(i)))
-        elif type(i)==list:
-            out.append((i,len(i)))
-        elif type(i)==float:
-            out.append((i,1))
-        elif type(i)==complex:
-            out.append((i,1))
-    print(out) 
-           
+    print(out)
+tupp(a)
+          
 
 
 
