7a81ba8f4e539b33f77bb237854cfeb1c2d28a71
Samir Shaw <mitchellsamir999@gmail.com>
2026-01-29 13:08:52 +0530

    108

--- 127.py
+++ 127.py
@@ -1,13 +1,23 @@
-#WAP to perform the sum of a and b with output in int
+#WAP to perform the sum of a and b with output in int using recursion
 a=['one','nine','four']
 b=['seven','zero','one']
-output=895
-a1=0
-sum=0
-for i in a:
+expected_output=895
 def rec(a,b,out=0):
-    dt={'one':1, 'nine':9, 'four':4,'seven':7,'zero':0,'one':1}
-    for i in range(-1,-3,-1):
-        sum=
+    dt={'zero':'0,'one':1,'two':2,'three':3,'four':4,'five':5,'six':6,'seven':7,'eight':8,'nine':9}
+   
+    
+
+
+
+
+
+
+# a1=0
+# sum=0
+# for i in a:
+# def rec(a,b,out=0):
+#     dt={'one':1, 'nine':9, 'four':4,'seven':7,'zero':0,'one':1}
+#     for i in range(-1,-3,-1):
+#         sum=
 
 
