8daa323c53364ab05aff9fb95fc1607a21761b1b
Samir Shaw <mitchellsamir999@gmail.com>
2026-01-30 19:50:47 +0530

    64646

--- 49.py
+++ 49.py
@@ -1,6 +1,7 @@
-#WAP if a number is divisible by 3 print square of it and
-# if num is divisible by 5 print cube of it 
-# and if divisible by both 3 and 5 print double of that number between 1 to 50.
+# WAP if a number is divisible by 3 print square of it 
+# and if num is divisible by 5 print cube of it 
+# and if divisible by both 3 and 5 
+# print double of that number between 1 to 50.
 i = 1
 while i < 51:
     if i % 3==0 and i % 5 == 0:
