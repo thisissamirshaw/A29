73acd9ddcc46f216e7397383bc643133777fe3fa
Samir Shaw <mitchellsamir999@gmail.com>
2026-01-30 19:53:01 +0530

    556546

--- 50.py
+++ 50.py
@@ -1,4 +1,5 @@
-#WAP to print num which are divisible by 5 between m and n
+# WAP to print num which are divisible by 5 
+# between m and n
 m = int(input("Enter a number:"))
 n = int(input("Enter a number:"))
 while m <= n:
